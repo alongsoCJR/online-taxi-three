d18082e74e9d307598726a3a980d0047b1587021
chenjianrong <chenjianrong@fenbi.com>
2021-10-30 15:14:02 +0800

    [add] 添加额外的文件

---
+++ api-base/src/test/java/com/mashibing/api/base/ApiBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashibing.api.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
