364d11ea104ae97708a834ca272f2a82c2db66f8
chenjianrong <chenjianrong@fenbi.com>
2021-11-08 22:32:13 +0800

    [add] 配置中心自动刷新（刷新单台应用，通过rabbitMq刷新多台应用）

--- consumer/src/main/java/com/mashibing/consumer/controller/BaseController.java
+++ consumer/src/main/java/com/mashibing/consumer/controller/BaseController.java
@@ -4,12 +4,14 @@ import com.mashibing.consumer.fallback.RestTemplateFallBack;
 import com.mashibing.consumer.service.ConsumerApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 @RestController
+@RefreshScope
 public class BaseController {
 
     @Autowired
